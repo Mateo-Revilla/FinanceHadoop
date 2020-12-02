4d6a2d41f4a56e273de8032de00c878655383983
mkiisa <marcuslukaskiisa@gmail.com>
2020-12-03 01:40:44 +0200

    test

--- covid/analysis/mapreduce/mapper3.py
+++ covid/analysis/mapreduce/mapper3.py
@@ -4,17 +4,17 @@ import sys
 import csv
 
 
-d = {}
+d = {
+    "cases" : 7,
+    "sp fluc" : 5,
+    "sp change" : 6,
+    "deaths" : 8
+}
 stock_arg = "sp fluc"
 covid_arg = "cases"
-print("%s,%s" % (covid_arg, stock_arg))
-for index, line in enumerate(csv.reader(sys.stdin)):
-    if index == 0:
-        for i, item in enumerate(line):
-            d[item] = i
 
-    if index != 0:
-        change_val = float(line[d[stock_arg]])
-        covid_val = int(line[d[covid_arg]])
-        print("%i,%f" % (covid_val,change_val))
+for index, line in enumerate(csv.reader(sys.stdin)):
+    change_val = float(line[d[stock_arg]])
+    covid_val = int(line[d[covid_arg]])
+    print("%i,%f" % (covid_val,change_val))
 
--- covid/analysis/mapreduce/reducer.py
+++ covid/analysis/mapreduce/reducer.py
@@ -6,8 +6,5 @@ import csv
 # extracting cases and change columns 
 lines = [i for i in csv.reader(sys.stdin)]
 for index, line in enumerate(lines):
-    if index == 0:
-        print(line[0] + ',' + line[1])
-        continue
     change_val = float(line[1])
     print('%i,%f' % (int(line[0]),change_val * 100))
