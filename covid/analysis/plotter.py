f38d8edef80c1927cf31830112c7809751582ee1
mkiisa <marcuslukaskiisa@gmail.com>
2020-12-03 05:01:39 +0200

    final working submission

--- covid/analysis/plotter.py
+++ covid/analysis/plotter.py
@@ -8,40 +8,51 @@ import numpy as np
 
 save = True
 
-l = [(line[0], line[1]) for line in csv.reader(sys.stdin)]
-covid_arg, stock_arg = l[0]
-l = l[1:]
-x_values = [i for i in range(len(l))]
+def get_labels(number):
+    if number == 1:
+        return "cases", " %% growth"
+    if number == 2:
+        return "deaths", " %% growth"
+    if number == 3:
+        return "cases", " %% fluctuation"
+    if number == 4:
+        return "deaths", " %% fluctuation"
+
+
+if len(sys.argv) == 2:
+    covid_val, stock_val = get_labels(int(sys.argv[1]))
+else:
+    raise ValueError("Pass in number 1-4 as argument that matches the job number. e.g 'cat job1 | python plotter.py 1'.")
+
+data = sorted([(int(line[0]), float(line[1])) for line in csv.reader(sys.stdin)], key=lambda x: x[0])
+
+
+x_values = [i for i in range(len(data))]
 scaler = MinMaxScaler(feature_range=(0,10))
 
-cases = scaler.fit_transform(np.array([int(i[0]) for i in l]).reshape(-1, 1))
-change = np.array([float(i[1]) for i in l]).reshape(-1, 1)
+cases = scaler.fit_transform(np.array([i[0] for i in data]).reshape(-1, 1))
+change = np.array([i[1] for i in data]).reshape(-1, 1)
 
 plt.plot(x_values,change, 'b')
 plt.plot(x_values,cases, 'g')
 
 
-if stock_arg == "sp fluc":
-    stock_val = "fluctuation"
-elif stock_arg == "sp change":
-    stock_val = "change"
-
-
-plt.legend(("S&P 500 %s" % stock_val,"COVID %s (scaled to values between 0, 10" % covid_arg))
-plt.plot(x_values,np.zeros(len(l)),'r--')
-plt.title("S&P500 %s vs Total COVID %s in the US" % (covid_arg, stock_val))
+plt.legend(("S&P 500 %s" % stock_val,"COVID %s (scaled to values between 0, 10" % covid_val))
+plt.plot(x_values,np.zeros(len(data)),'r--')
+plt.title("S&P500 %s vs Total COVID %s in the US" % (stock_val, covid_val))
 
 plt.xlabel("Trading Days")
 plt.ylabel("Percent change")
 
 if save:
-    plt.savefig(covid_arg + '-' + stock_val + '.png')
+    plt.savefig('results/' + 'chart' + sys.argv[1] + '.png')
 else:
     plt.show()
 
 
 
 
+    
 
 
 
